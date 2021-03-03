a338bcec15ef3afe3e0fa43da8ad1099f4c71e23
jogger <980328722@qq.com>
2021-03-03 18:13:06 +0800

    first commit

--- album_helper/src/main/java/com/example/album_helper/ui/SelectImageActivity.kt
+++ album_helper/src/main/java/com/example/album_helper/ui/SelectImageActivity.kt
@@ -1,17 +1,23 @@
 package com.example.album_helper.ui
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.album_helper.R
 import com.example.album_helper.helper.AlbumHelper
+import com.example.album_helper.model.ImageFolder
 import com.example.album_helper.ui.adapter.ImageSelectAdapter
 import com.example.album_helper.ui.viewmodel.SelectImageViewModel
 import com.example.album_helper.util.getViewModel
 import kotlinx.android.synthetic.main.activity_select_image.*
+import java.util.ArrayList
 
 class SelectImageActivity:BaseImageActivity<SelectImageViewModel>(){
 private val albumHelper=AlbumHelper.get()
+    private val  adapter:ImageSelectAdapter by lazy { ImageSelectAdapter(this).apply {
+
+    } }
     override val viewModel: SelectImageViewModel
         get() = getViewModel(SelectImageViewModel::class.java)
 
@@ -19,10 +25,18 @@ private val albumHelper=AlbumHelper.get()
 
     override fun initView(savedInstanceState: Bundle?) {
         rv_content.layoutManager=GridLayoutManager(this,3,RecyclerView.VERTICAL,true)
-        rv_content.adapter=ImageSelectAdapter(this).apply {
-
-        }
+        rv_content.adapter=adapter
+        viewModel.getImageFoldersLiveData().observe(this, Observer { updateView(it) })
         viewModel.loadImageDatas(this,albumHelper)
+    }
+
+    private fun updateView(it: ArrayList<ImageFolder>?) {
+        if (it!=null&&it.size!=0) {
+            val imageFolder = it.get(0)
+            adapter.setItems(imageFolder.images)
+        }
+
+
     }
 
 }
