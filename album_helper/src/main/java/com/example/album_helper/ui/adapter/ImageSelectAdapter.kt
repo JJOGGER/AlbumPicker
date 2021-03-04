0a14be586850d4230213743915c969a7decfa21d
Jjogger <980328722@qq.com>
2021-03-04 23:51:46 +0800

    图片获取

--- album_helper/src/main/java/com/example/album_helper/ui/adapter/ImageSelectAdapter.kt
+++ album_helper/src/main/java/com/example/album_helper/ui/adapter/ImageSelectAdapter.kt
@@ -6,6 +6,7 @@ import com.example.album_helper.model.ImageItem
 import com.example.album_helper.util.deviceWidth
 import com.horizon.doodle.Doodle
 import kotlinx.android.synthetic.main.rv_image_select_item.view.*
+import load
 
 class ImageSelectAdapter(context:Context): SimpleRecyclerAdapter<ImageItem>(context, R.layout.rv_image_select_item) {
     private val imageSize = deviceWidth(context) / 4
@@ -17,7 +18,7 @@ class ImageSelectAdapter(context:Context): SimpleRecyclerAdapter<ImageItem>(cont
 //            iv_image.loadAny(item.path)
 //            Doodle.load(item.path!!)
 //                .into(iv_image)
-            iv_image.load
+            iv_image.load(item.path)
 //            iv_image.setImageBitmap(BitmapFactory.decodeFile(item.path))
         }
     }
